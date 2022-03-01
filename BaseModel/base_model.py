0c6b362505f7b40835b7d3be2cad0694d782a21d
AtaharNurPial <atp82232@gmail.com>
2022-03-01 12:44:17 +0600

    updated basemodel

--- BaseModel/base_model.py
+++ BaseModel/base_model.py
@@ -1,40 +1,34 @@
-# import pydantic
+import pydantic
 from pydantic import BaseModel
 from typing import List,Any
 
-RESTAURENT_PREFIX = '#RESTAURENT#'
+RESTAURANT_PREFIX = '#RESTAURANT#'
 ITEM_PREFIX = '#ITEM#'
 
 class Variation(BaseModel):
     name: str = ''
-    price:str = ''
-    currency:str = ''
+    price: str = ''
+    currency: str = ''
 
 class Menu_Item(BaseModel):
-    PK:str = ''
+    PK: str = ''
     SK: str = ''
     restaurant_id: str 
     item_id: str
     item_name: str
-    ingredients: List[str] = []
+    ingedients: List[str] = []
     variations: List[Variation] = []
     image: str = ''
     category: str = ''
 
     def __init__(self, **data: Any) -> None:
         super().__init__(**data)
-        self.PK = f"{RESTAURENT_PREFIX}{self.restaurant_id}"
+        self.PK = f"{RESTAURANT_PREFIX}{self.restaurant_id}"
         self.SK = f"{ITEM_PREFIX}{self.item_id}"
-        
+
 test_variation = Variation(name= 'botella', price='33',currency='$')
 test_menuItem = Menu_Item(restaurant_id='1235',item_id='b23',item_name='testItem',
                 ingredients=['air','water','fire','earth'],
                 variations=[test_variation],image='testImage',category='Food')
 
 print(f"test_Model: {test_menuItem}")
-
-
-
-
-
-
