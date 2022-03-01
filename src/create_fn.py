821659c14b9f713ba1662db3884ed0502ca6b9a9
AtaharNurPial <atp82232@gmail.com>
2022-03-01 14:11:23 +0600

    initial template and function body

---
+++ src/create_fn.py
@@ -0,0 +1,15 @@
+
+import json
+
+def lambda_handler(event, context):
+    print(event)
+    return{
+        'statusCode': 200,
+        'headers': {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+        },
+        'body': json.dumps({
+            'message': 'hello from create function'
+        })
+    }
